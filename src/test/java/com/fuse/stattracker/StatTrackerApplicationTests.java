301c8462e1e4c354ba1b181af5320e50dc70017b
Seth  Pennebaker <createdbyfuse@Seths-MacBook-Air.local>
2024-02-02 20:46:29 -0500

    Initial commit

---
+++ src/test/java/com/fuse/stattracker/StatTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuse.stattracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
